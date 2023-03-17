69dff9590ecae62ba5ccfc62cb8af98cda4789fc
P1X3L_R4 <p1x3lr4@gmail.com>
2023-03-17 08:42:02 -0300

    Update 01

--- components/Carrinho.js
+++ components/Carrinho.js
@@ -1,37 +1,72 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {Text, View, StyleSheet, ScrollView} from 'react-native';
 import Produto from './Produto';
+import PlacaVideoImage from '../image/placa-video.png';
+import ProcessadorImage from '../image/processador.png';
+import WaterCooler from '../image/water-cooler.png';
 
 const produto = [
-  {nome: 'Mouse', preco: 12.3},
-  {nome: 'Teclado', preco: 20},
-  {nome: 'Telefone', preco: 250},
+  {
+    image: PlacaVideoImage,
+    nome: 'Placa de Vídeo RTX 3060 Asus Dual O12G V2 NVIDIA GeForce',
+    descricao:
+      'Tenha um desempenho para seu trabalho e gameplay com a Placa de Vídeo Asus NVIDIA GeForce DUAL RTX 3060 O12G V2 é equipada com uma memória de 12GB GDDR6, Velocidade de Memória de 15 Gbps, e Boost Clock de 1867 MHz em OC e de 1867 MHz no modo Jogo.',
+    preco: 2.449,
+  },
+  {
+    image: ProcessadorImage,
+    nome: 'Processador AMD Ryzen 5 5600G, 3.9GHz (4.4GHz Max Turbo)',
+    descricao:
+      'Esteja você jogando os jogos mais recentes, projetando o próximo arranha-céu ou analisando dados científicos, a velocidade sem precedentes dos processadores AMD Ryzen série 5000 G para desktop é imparável. Com os processadores AMD Ryzen para desktop, você está sempre na frente.',
+    preco: 819.99,
+  },
+  {
+    image: WaterCooler,
+    nome: 'Processador AMD Ryzen 5 5600G, 3.9GHz (4.4GHz Max Turbo)',
+    descricao:
+      'Esteja você jogando os jogos mais recentes, projetando o próximo arranha-céu ou analisando dados científicos, a velocidade sem precedentes dos processadores AMD Ryzen série 5000 G para desktop é imparável. Com os processadores AMD Ryzen para desktop, você está sempre na frente.',
+    preco: 819.99,
+  },
 ];
 
 class Carrinho extends React.Component {
-  
-      state = {
-        valorTotal: 0
-      };
+  state = {
+    valorTotal: 0,
+  };
 
-      atualizarTotal(valor){
-        this.setState({
-          valorTotal: this.state.valorTotal + valor
-        })
-      }
-      
+  atualizarTotal(valor) {
+    this.setState({
+      valorTotal: this.state.valorTotal + valor,
+    });
+  }
+
   render() {
     return (
-      <View>
-        <Produto p={produto[0]} valorFinal={this.atualizarTotal.bind(this)}/>
-        <Produto p={produto[1]} valorFinal={this.atualizarTotal.bind(this)}/>
-        <Produto p={produto[2]} valorFinal={this.atualizarTotal.bind(this)}/>
-        <Text>
-          {this.state.valorTotal}
-        </Text>
-      </View>
+      <ScrollView>
+        <View style={styles.sectionContainer}>
+          <Produto p={produto[0]} valorFinal={this.atualizarTotal.bind(this)} />
+          <Produto p={produto[1]} valorFinal={this.atualizarTotal.bind(this)} />
+          <Produto p={produto[2]} valorFinal={this.atualizarTotal.bind(this)} />
+          {/* <Text>Valor Final: {this.state.valorTotal}</Text> */}
+        </View>
+
+        <View style={styles.previwerBuy}>
+          <Text>Produto: {this.state.quantidade}</Text>
+          <Text>Quantidade: {this.state.quantidade}</Text>
+          <Text>Valor Total: {this.state.quantidade}</Text>
+        </View>
+      </ScrollView>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    flex: 1,
+    flexDirection: 'column',
+    rowGap: 10,
+    padding: 10,
+  },
+});
 
 export default Carrinho;
--- components/Produto.js
+++ components/Produto.js
@@ -1,4 +1,4 @@
-import {Button, Text, View} from 'react-native';
+import {Button, Text, View, Image, StyleSheet} from 'react-native';
 import React from 'react';
 
 class Produto extends React.Component {
@@ -16,22 +16,63 @@ class Produto extends React.Component {
       qtd: this.state.qtd + 1,
       total: this.state.total + this.props.p.preco,
     });
+    this.props.valorFinal(this.props.p.preco);
   }
   render() {
-    return (
-      <View>
-        <Text>{this.props.p.nome}</Text>
-        <Text>Preço: R${this.props.p.preco}</Text>
-        <Text>Quantidade: {this.state.qtd}</Text>
-        <Text>Total: R$ {this.state.total}</Text>
 
-        <Button
-          title={this.state.adicionado ? 'Adicionado' : 'Adicionar'}
-          onPress={this.addProduto.bind(this)}
-        />
+    var curso = "Sistema de Informação";
+    return (
+      <View style={styles.cardProduct}>
+        <Text style={styles.titleProduct}>{this.props.p.nome}</Text>
+        <Image source={this.props.p.image} style={styles.imageProduct} />
+        <Text style={styles.descProduct}>
+          {this.props.p.descricao}
+        </Text>
+        <Text style={styles.precoProduct}>Preço: {this.props.p.preco}</Text>
+        <Button title='Adicionar ao Carrinho' />
+        {/* <Text>Quantidade: {this.state.qtd}</Text>
+          <Text>Total: R$ {this.state.total}</Text> */}
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  cardProduct: {
+    width: '100%',
+    flex: 1,
+    flexDirection: 'column',
+    alignItems: 'center',
+    borderWidth: 2.5,
+    borderRadius: 5,
+    borderColor: '#ff830f',
+    borderStyle: 'solid',
+    padding: 12
+  },
+  imageProduct: {
+    borderWidth: 2,
+    width: 150,
+    height: 100,
+    marginBottom: 25,
+  },
+  titleProduct: {
+    fontSize: 18,
+    fontWeight: '700',
+    marginBottom: 20,
+    color: '#8a4f17',
+  },
+  precoProduct: {
+    fontSize: 20,
+    fontWeight: '900',
+    margin: 20,
+    backgroundColor: '#ff830f',
+    padding: 12,
+    borderRadius: 5,
+
+  },
+  descProduct:{
+    color: '#000'
+  }
+});
 
 export default Produto;
